9ba8a8d12d505996ea51b50e195af6842bf0c659
Kripasindhu <krips.from.iit.kgp@gmail.com>
2015-10-07 11:57:49 +0200

    Removed SVM light source, added required cmake options

--- cmake/od_mandatory_dependency.cmake
+++ cmake/od_mandatory_dependency.cmake
@@ -4,4 +4,5 @@ find_package(VTK REQUIRED)
 find_package(Eigen REQUIRED)
 find_package(Boost 1.40 COMPONENTS program_options REQUIRED )
 
+
 include_directories("${OD_SOURCE_DIR}" ${EIGEN_INCLUDE_DIRS} ${OpenCV_INCLUDE_DIRS} ${PCL_INCLUDE_DIRS} ${OD_SOURCE_DIR}/3rdparty/SiftGPU/src/SiftGPU)
